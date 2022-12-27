ff9f8980441aacb7c18aec598bd1bc35a627104e
JeevanPrasanthS <121091454+JeevanPrasanthS@users.noreply.github.com>
2022-12-27 12:04:18 +0530

    Add files via upload

---
+++ EmployeeResitration/src/main/java/com/Ust/EmployeeResitration/EmployeeResitrationApplication.java
@@ -0,0 +1,13 @@
+package com.Ust.EmployeeResitration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeResitrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeResitrationApplication.class, args);
+	}
+
+}
---
+++ EmployeeResitration/src/test/java/com/Ust/EmployeeResitration/EmployeeResitrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ust.EmployeeResitration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeResitrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
